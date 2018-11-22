23229cb1b55987e902756c65c69e0779c1a7ab15
CuiRa <ryu.pino@ezweb.ne.jp>
2018-11-22 11:25:13 +0900

    サマリーの追加

--- src/Game/Character/Player.h
+++ src/Game/Character/Player.h
@@ -15,19 +15,38 @@ namespace GFF
 {
 namespace Character
 {
+    /// <summary>
+    /// プレイヤークラス
+    /// </summary>
     class Player : public ICharacter
     {
         OBJECT2D_PARENT(Player, ICharacter);
 
     public:
 
+        /// <summary>
+        /// 発射処理
+        /// </summary>
         virtual void Shoot() override;
-        virtual void Start() override;
+
+        /// <summary>
+        /// 更新処理
+        /// </summary>
+        /// <param name="_elapsedTime">前フレームからの経過時間</param>
         virtual void Update(float _elapsedTime) override;
 
     private:
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_elapsedTime">前フレームからの経過時間</param>
         virtual void Move(float _elapsedTime) override;
+
+        /// <summary>
+        /// 開始処理
+        /// </summary>
+        virtual void Start() override;
     };
 
 } // Character
