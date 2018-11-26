fde07db1f46ae543f62517f9472a7dd054cc4aad
AkiraSekine <ekusika834@gmail.com>
2018-11-26 11:22:19 +0900

    敵の移動を作成 左に進むだけ

--- src/Game/Character/Enemy.cpp
+++ src/Game/Character/Enemy.cpp
@@ -7,20 +7,42 @@
 
 #include "Enemy.h"
 
+#include "Math/Collider2D.h"
+
+#include "Player.h"
+
+using namespace CreaDXTKLib::Math;
+
+using namespace GFF::System;
+
 namespace GFF
 {
 namespace Game
 {
 namespace Character
 {
-    //Player* Enemy::m_player;
+    Player* Enemy::m_player;
 
     void Enemy::Update(float _elapsedTime)
     {
         Move(_elapsedTime);
     }
 
+    void Enemy::SetPlayer(Player * _player)
+    {
+        m_player = _player;
+    }
+
     void Enemy::Move(float _elapsedTime)
+    {
+        Vector2 pos = Position();
+
+        pos += -GetRightVector() * gunData.moveSpeed * _elapsedTime;
+
+        Position(pos);
+    }
+
+    void Enemy::Start()
     {
         
     }
