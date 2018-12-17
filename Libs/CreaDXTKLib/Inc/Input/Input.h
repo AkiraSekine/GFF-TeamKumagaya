0605d717b1f71e685959f2089186600e4e8ce036
AkiraSekine <ekusika834@gmail.com>
2018-12-17 12:00:48 +0900

    ライブラリの更新

--- Libs/CreaDXTKLib/Inc/Input/Input.h
+++ Libs/CreaDXTKLib/Inc/Input/Input.h
@@ -21,11 +21,40 @@ namespace Input
     /// </summary>
     enum struct MouseButtons : char
     {
-        Left,
-        Middle,
-        Right,
-        Button1,
-        Button2
+        Left,    // 左クリック
+        Middle,  // 中央ボタン
+        Right,   // 右クリック
+        Button1, // 戻るボタン
+        Button2  // 進むボタン
+    };
+
+    /// <summary>
+    /// コントローラパーツ
+    /// </summary>
+    enum struct ControllerParts : char
+    {
+        A,          // Aボタン
+        B,          // Bボタン
+        X,          // Xボタン
+        Y,          // Yボタン
+        LShoulder,  // 左ショルダー
+        RShoulder,  // 右ショルダー
+        Up,         // 十字上ボタン
+        Down,       // 十字下ボタン
+        Left,       // 十字左ボタン
+        Right,      // 十字右ボタン
+        Start,      // スタートボタン
+        Menu,       // メニューボタン
+        Back,       // 戻るボタン
+        View,       // ビューボタン
+        LStick,     // 左スティックボタン
+        RStick,     // 右スティックボタン
+        LTrigger,   // 左トリガー
+        RTrigger,   // 右トリガー
+        LStickX,    // 左スティックX方向
+        LStickY,    // 左スティックY方向
+        RStickX,    // 右スティックX方向
+        RStickY     // 右スティックY方向
     };
 
     /// <summary>
@@ -40,12 +69,12 @@ namespace Input
         /// <summary>
         /// チェック方法
         /// </summary>
-        enum struct CheckMode : char
+        enum struct CheckMode : unsigned char
         {
-            Down    = 0b0001,    //押した瞬間
-            Up      = 0b0010,    //離した瞬間
-            Press   = 0b0100,    //押している間
-            Release = 0b1000     //離している間
+            Down    = 0b0001,    // 押した瞬間
+            Up      = 0b0010,    // 離した瞬間
+            Press   = 0b0100,    // 押している間
+            Release = 0b1000     // 離している間
         };
 
         /// <summary>
@@ -70,6 +99,28 @@ namespace Input
             const MouseButtons& _button,
             const float& _value,
             const CheckMode& _mode);
+        /// <summary>
+        /// 入力設定を追加
+        /// </summary>
+        /// <param name="_name">ハンドル名</param>
+        /// <param name="_num">コントローラ番号</param>
+        /// <param name="_parts">コントローラパーツ</param>
+        /// <param name="_value">値</param>
+        /// <param name="_mode">チェック方法</param>
+        void Add(const std::wstring& _name,
+            const int& _num,
+            const ControllerParts& _parts,
+            const float& _value,
+            const CheckMode& _mode);
+        /// <summary>
+        /// 入力設定を追加
+        /// </summary>
+        /// <param name="_name">ハンドル名</param>
+        /// <param name="_num">コントローラ番号</param>
+        /// <param name="_parts">コントローラパーツ</param>
+        void Add(const std::wstring& _name,
+            const int& _num,
+            const ControllerParts& _parts);
 
         /// <summary>
         /// 入力設定を削除
@@ -88,14 +139,21 @@ namespace Input
 
         struct InputSet
         {
-            bool isKey;
+            enum
+            {
+                Keyboard,
+                Mouse,
+                Controller
+            } source;
 
             union
             {
                 DirectX::Keyboard::Keys key;
                 MouseButtons mouseButton;
+                ControllerParts parts;
             } keyButton;
 
+            int num;
             float value;
             CheckMode checkMode;
         };
